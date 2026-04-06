0be300b6f0e016caa12ebd379c7a87a303bc4a87
WrublTop <maciek74majgier@gmail.com>
2026-04-06 06:02:14 +0200

    feat(core): add foundational types and interfaces
     - ExoCodes: ANSI escape code helpers
      - GameTime, InputEvent: core data structs
      - IRenderBuffer, IScreen: engine contracts

---
+++ Core/ExoCodes.cs
@@ -0,0 +1,14 @@
+﻿namespace ExoProxy.Core;
+
+public static class ExoCodes
+{
+    private const string Esc = "\e[";
+
+    public static string Reset => "\e[0m";
+    public static string ClearScreen => "\e[2J";
+    public static string HideCursor => "\e[?25l";
+    public static string ShowCursor => "\e[?25h";
+    public static string MoveTo(int x, int y) => $"\e[{y};{x}H";
+    public static string Fg(int r, int g, int b) => $"\e[38;2;{r};{g};{b}m";
+    public static string Bg(int r, int g, int b) => $"\e[48;2;{r};{g};{b}m";
+}
---
+++ Core/IRenderBuffer.cs
@@ -0,0 +1,12 @@
+﻿namespace ExoProxy.Core;
+
+public interface IRenderBuffer
+{
+    int Width { get; }
+    int Height { get; }
+    void WriteAt(int x, int y, string text);
+    void WriteAt(int x, int y, string text, string ansiColor);
+    void WriteRaw(int x, int y, string rawAnsiString);
+    void Clear();
+    string Flush();
+}
